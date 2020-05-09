28ad237c79092452cba779ae04af26145497409a
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-05-09 11:29:43 +0100

    Updates

---
+++ precache-manifest.e1981300f280d81aec74da237177f03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f115e416167b74e9b7f70dd923e0ca",
+    "url": "/gepakt-en-gezakt/index.html"
+  },
+  {
+    "revision": "5f3a5fe5d4b88430c85c",
+    "url": "/gepakt-en-gezakt/static/css/main.aaafea42.chunk.css"
+  },
+  {
+    "revision": "7d70820fc2ef4e7a8014",
+    "url": "/gepakt-en-gezakt/static/js/2.f0500d92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gepakt-en-gezakt/static/js/2.f0500d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3a5fe5d4b88430c85c",
+    "url": "/gepakt-en-gezakt/static/js/main.333b81ea.chunk.js"
+  },
+  {
+    "revision": "1bd1e47c393a735a5a2d",
+    "url": "/gepakt-en-gezakt/static/js/runtime-main.0037d2d8.js"
+  }
+]);
